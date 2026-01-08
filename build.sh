2f2ac596bc1ace79632b2fcd832ad5b57997df53
virendra-978 <virendrat2121@gmail.com>
2026-01-08 10:32:45 +0530

    updated build

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ echo "Compiling source files..."
 find src -name "*.java" > sources.txt 
 javac @sources.txt 
 echo "Compilation complete." 
-echo "=== Running Tests ==="java -cp src/main/java com.example.HelloDevOpsTest 
+echo "=== Running Tests ==="
+java -cp src/main/java com.example.HelloDevOpsTest 
 echo "Tests completed." 
 echo "=== Creating JAR ===" 
 jar cfe app.jar com.example.HelloDevOps -C src/main/java . 
